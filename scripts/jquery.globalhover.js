c060532df5bb81ab4af7c179bdd8dd43557a47fd
Сергей Моисеев <moiseev@itpc.ru>
2017-04-07 12:55:06 +0500

    buxfixes

--- scripts/jquery.globalhover.js
+++ scripts/jquery.globalhover.js
@@ -1,3 +1,4 @@
+// Полифил для MouseEvent
 (function(window)
 {
 	try { new MouseEvent('test'); }
@@ -9,7 +10,7 @@
 		params = params || { bubbles: false, cancelable: false };
 		var mouseEvent = document.createEvent('MouseEvent');
 		mouseEvent.initMouseEvent(eventType, params.bubbles, params.cancelable, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-
+		
 		return mouseEvent;
 	}
 
@@ -19,57 +20,72 @@
 
 (function($)
 {
-	/*
+	/**
 	* GlobalHover plugin v2017-01-30
 	* Проверялось на JQuery 3.1.1
 	*/
-	
+	let watchlist = [];
 	let global_dispatch_enabled = true;
+	
+	let $prev_hovered_element;
+	
+	let $findHoveredElement = function(mouse_event)
+	{
+		let e = mouse_event;
+		let $hovered_element;
+		
+		watchlist.forEach(function($element)
+		{
+			let top    = $element.offset().top;
+			let left   = $element.offset().left;
+			let bottom = top + $element.height();
+			let right  = left + $element.width();
+			
+			let is_mouseover = (e.pageY >= top && e.pageY <= bottom && e.pageX >= left && e.pageX <= right);
+			
+			if (is_mouseover)
+			{
+				$hovered_element = $element;
+				return;
+			}
+		});
+		
+		return $hovered_element;
+	};
+	
+	
+	$(document).mousemove(function(e)
+	{
+		let $hovered_element = $findHoveredElement(e);
+		
+		if (global_dispatch_enabled && $hovered_element !== $prev_hovered_element)
+		{
+			if ($hovered_element)
+				$hovered_element[0].dispatchEvent(new MouseEvent("global-mouseenter", { bubbles: false, cancelable: true }));
+			
+			if ($prev_hovered_element)
+				$prev_hovered_element[0].dispatchEvent(new MouseEvent("global-mouseleave", { bubbles: false, cancelable: true }));
+		}
+		
+		$prev_hovered_element = $hovered_element;
+	});
+	
+	
 	
 	$.fn.globalHover = function(in_handler, out_handler)
 	{
 		return this.each(function()
 		{
-			let self = $(this);
-			// console.log(this);
-			
-			let top    = self.offset().top;
-			let left   = self.offset().left;
-			let bottom = top + self.height();
-			let right  = left + self.width();
-			
-			let element = this;
-			
-			let prev_mouseover = false;
-			let is_mouseover   = false;
-			
 			if (in_handler)
-				element.addEventListener("global-mouseenter", in_handler);
+				this.addEventListener("global-mouseenter", in_handler);
 			
 			if (out_handler)
-				element.addEventListener("global-mouseleave", out_handler);
+				this.addEventListener("global-mouseleave", out_handler);
 			
-			$(document).mousemove(function(e)
-			{
-				// $element.text(e.pageX + " " + e.pageY);
-				if (!global_dispatch_enabled) return false;
-				
-				is_mouseover = (e.pageY >= top && e.pageY <= bottom && e.pageX >= left && e.pageX <= right);
-				
-				if (prev_mouseover != is_mouseover)
-				{
-					// console.log(e);
-					let event_name = is_mouseover
-						? "global-mouseenter"
-						: "global-mouseleave";
-					
-					element.dispatchEvent(new MouseEvent(event_name, {bubbles: false, cancelable: true}));
-				}
-				
-				prev_mouseover = is_mouseover;
-			});
+			watchlist.push($(this));
 		});
 	};
+	
 	
 	$.fn.globalHover.disableDispatch = function()
 	{
@@ -80,4 +96,6 @@
 	{
 		global_dispatch_enabled = true;
 	};
+	
+	
 }(jQuery));
